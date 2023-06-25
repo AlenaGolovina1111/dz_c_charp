958c38ed80f94934241228520cd750d04f6f6248
you@example.com <you@example.com>
2023-06-25 23:42:29 +0300

    save

---
+++ Семинар 1/SeminarTask1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая на вход принимает число больше 10  
+// и выдает его квадрат
+
+////исходное число
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()); 
+
+if (a <=10){
+    Сonsole.WriteLine("Ваше число не соответствует условию: число больше10");
+}
+else
+{
+    
+// результат квадрата исходного числа
+int result = a * a;
+
+// вывод результата 
+Console.WriteLine($"Квадрат вашего числа: = {result}");
+}
---
+++ Семинар 1/ДЗ_Задача_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите программу, которая на входе принимает 2 числа и выдает какое больше, какое меньше
+Console.WriteLine("Введите 2-а числа: ");
+int a, b;
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+if (a<=b);
+{
+ Сonsole.WriteLine($"{a} - Min; {b} - Max");
+}
+else (a>b);
+{
+Console.WriteLine($"{b} - Min; {a} - Max");
+}
+
+ 
