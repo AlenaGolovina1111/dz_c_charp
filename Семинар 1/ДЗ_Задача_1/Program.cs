7481e96c7731d1d7f3f92619038afffced199b7f
you@example.com <you@example.com>
2023-06-26 00:01:46 +0300

    save

--- Семинар 1/ДЗ_Задача_1/Program.cs
+++ Семинар 1/ДЗ_Задача_1/Program.cs
@@ -1,15 +1,29 @@
 ﻿// напишите программу, которая на входе принимает 2 числа и выдает какое больше, какое меньше
-Console.WriteLine("Введите 2-а числа: ");
-int a, b;
-a = int.Parse(Console.ReadLine());
-b = int.Parse(Console.ReadLine());
-if (a<=b);
+//Console.WriteLine("Введите 2-а числа: ");
+//int a, b;
+//a = int.Parse(Console.ReadLine());
+//b = int.Parse(Console.ReadLine());
+//if (a<=b);
+//{
+//Сonsole.WriteLine($"{a} - Min; {b} - Max");
+//}
+//else if (a>b);
+//{
+//Console.WriteLine($"{b} - Min; {a} - Max");
+//}
+class Program
 {
- Сonsole.WriteLine($"{a} - Min; {b} - Max");
-}
-else (a>b);
-{
-Console.WriteLine($"{b} - Min; {a} - Max");
-}
-
- 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        int a = int.Parse(Console.ReadLine());
+       
+        Console.WriteLine("Введите второе число:");
+        int b = int.Parse(Console.ReadLine());
+       
+        if (a > b)
+        {
+            Console.WriteLine($"Число {a} больше числа {b}");
+        }
+        else if (a < b)
+        {
